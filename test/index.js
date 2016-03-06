2f9afa8e35ea9e0d84eaad660f5b2536e52c4fbb
Caio Gondim <me@caiogondim.com>
2016-03-06 16:03:12 +0100

    Add basic test suite

---
+++ test/index.js
@@ -0,0 +1,139 @@
+'use strict'
+
+const tap = require('tap')
+const memoize = require('../src')
+
+tap.test('memoized function should be faster than original', (test) => {
+  // Vanilla Fibonacci
+
+  function vanillaFibonacci(n) {
+    return n < 2 ? n: vanillaFibonacci(n - 1) + vanillaFibonacci(n - 2)
+  }
+
+  const vanillaExecTimeStart = Date.now()
+  vanillaFibonacci(35)
+  const vanillaExecTime = Date.now() - vanillaExecTimeStart
+
+  // Memoized
+
+  function fibonacci(n) {
+    return n < 2 ? n: fibonacci(n - 1) + fibonacci(n - 2)
+  }
+
+  fibonacci = memoize(fibonacci)
+  const memoizedFibonacci = fibonacci
+
+  const memoizedExecTimeStart = Date.now()
+  memoizedFibonacci(35)
+  const memoizedExecTime = Date.now() - memoizedExecTimeStart
+
+  // Assertion
+
+  test.ok((memoizedExecTime < vanillaExecTime))
+  test.end()
+})
+
+tap.test('memoize functions with single arguments', (test) => {
+  function plusPlus(number) {
+    return number + 1
+  }
+
+  const memoizedPlusPlus = memoize(plusPlus)
+
+  // Assertions
+
+  test.equal(memoizedPlusPlus(1), 2)
+  test.equal(memoizedPlusPlus(1), 2) // Hits the cache
+  test.equal(memoizedPlusPlus(2), 3)
+  test.equal(memoizedPlusPlus(2), 3) // Hits the cache
+  test.end()
+})
+
+tap.test('memoize functions with N arguments', (test) => {
+    function nToThePower(n, power) {
+      return Math.pow(n, power)
+    }
+
+    const memoizedNToThePower = memoize(nToThePower)
+
+    // Assertions
+
+    test.equal(memoizedNToThePower(2, 3), 8)
+    test.equal(memoizedNToThePower(2, 3), 8) // Hits the cache
+    test.equal(memoizedNToThePower(3, 2), 9)
+    test.equal(memoizedNToThePower(3, 2), 9) // Hits the cache
+    test.end()
+})
+
+tap.test('inject custom cache', (test) => {
+  'use strict'
+
+  let hasMethodExecutionCount = 0
+  let setMethodExecutionCount = 0
+
+  // a custom cache instance must implement:
+  // - has
+  // - get
+  // - set
+  // - delete
+  class CustomCache {
+    constructor() {
+      this._cache = Object.create(null)
+      this._name = 'Object'
+    }
+
+    has(key) {
+      hasMethodExecutionCount++
+      return (key in this._cache)
+    }
+
+    get(key) {
+      return this._cache[key]
+    }
+
+    set(key, value) {
+      setMethodExecutionCount++
+      this._cache[key] = value
+    }
+
+    delete(key) {
+      delete this._cache[key]
+    }
+  }
+
+  function minus(a, b) {
+    return a - b
+  }
+
+  const memoizedMinus = memoize(minus, CustomCache)
+  memoizedMinus(3, 1)
+  memoizedMinus(3, 1)
+
+  // Assertions
+
+  test.equal(hasMethodExecutionCount, 2)
+  test.equal(setMethodExecutionCount, 1)
+  test.end()
+})
+
+tap.test('inject custom serializer', (test) => {
+  let serializerMethodExecutionCount = 0
+
+  function serializer() {
+    serializerMethodExecutionCount++
+    return JSON.stringify(arguments)
+  }
+
+  function minus(a, b) {
+    return a - b
+  }
+
+  const memoizedMinus = memoize(minus, null, serializer)
+  memoizedMinus(3, 1)
+  memoizedMinus(3, 1)
+
+  // Assertions
+
+  test.equal(serializerMethodExecutionCount, 2)
+  test.end()
+})
