49f0e1af6b416debd0e5c9ed0a2182277ca1fe68
Caio Gondim <me@caiogondim.com>
2016-06-17 22:40:05 +0200

    SauceLabs + Karma working

--- karma.conf.js
+++ karma.conf.js
@@ -14,25 +14,14 @@ var sauceLabsConf = {
     sl_chrome: {
       base: 'SauceLabs',
       browserName: 'chrome',
-      platform: 'Windows 7',
-      version: '35'
+      version: 'latest',
+      platform: 'Windows 10'
     },
     sl_firefox: {
       base: 'SauceLabs',
       browserName: 'firefox',
-      version: '30'
-    },
-    sl_ios_safari: {
-      base: 'SauceLabs',
-      browserName: 'safari',
-      platform: 'OS X 10.9',
-      version: '7.1'
-    },
-    sl_ie_11: {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 8.1',
-      version: '11'
+      version: 'latest',
+      platform: 'Windows 10'
     }
   }
 }
