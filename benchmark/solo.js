0585a8e1f6690ad6bafda1aecf16b5b0ddba9337
Caio Gondim <caio.gondim@nytimes.com>
2016-12-07 23:28:54 -0200

    Add `npm run benchmark:solo`

---
+++ benchmark/solo.js
@@ -0,0 +1,28 @@
+const debug = require('logdown')()
+const Benchmark = require('benchmark')
+const fastMemoize = require('../src')
+
+//
+// Fibonacci suite
+//
+
+const fibonacci = (n) => {
+  return n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+const memoizedFastMemoizeCurrentVersion = fastMemoize(fibonacci)
+
+
+const suiteFibonnaci = new Benchmark.Suite()
+const fibNumber = 15
+
+suiteFibonnaci
+  .add(`fast-memoize@current`, () => {
+    memoizedFastMemoizeCurrentVersion(fibNumber)
+  })
+  .on('cycle', (event) => {
+    const currentRunning = String(event.target)
+      .replace(/(.*)\ x/, (match, p1) => `\`${p1}\` x`)
+    debug.log(currentRunning)
+  })
+  .run({'async': true})
