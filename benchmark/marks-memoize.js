21c542749bc305a8299fe2645ed6536e4f061ecd
Mark Holmes <smlmrkhlms@gmail.com>
2017-01-26 15:17:42 -0500

    Added isPrimitive Check

--- benchmark/marks-memoize.js
+++ benchmark/marks-memoize.js
@@ -1,8 +1,11 @@
-function memoize(fn, monadic = fn.length === 1, cache = {}) {
-  return function memoized(...args) {
-    const key = monadic ? args[0] : JSON.stringify(args)
+function memoize(fn, cache = {}, monadic = fn.length === 1) {
+
+  function isPrimitive(value) { return value == null || (typeof value !== 'function' && typeof value !== 'object') }
+
+  return function memoized(first, ...args) {
+    const key = monadic && isPrimitive(first) ? first : JSON.stringify([first, ...args])
     if (cache[key]) { return cache[key] }
-    return cache[key] = fn(...args)
+    return cache[key] = fn(first, ...args)
   }
 }
 
